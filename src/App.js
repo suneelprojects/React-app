9163147711fd45f626b2a315ee061aa13c05ed67
Suneel Kumar <138564160+suneelprojects@users.noreply.github.com>
2025-12-19 07:27:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>React App!!
+          Edit <code>src/App.js</code>This is the 2nd Version of our App!!
         </p>
         <a
           className="App-link"
